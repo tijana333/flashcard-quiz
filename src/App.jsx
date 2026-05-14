f4b8213649fe9dd7ca7cb06b8cfaf40ddcfe4349
tijana333 <150601653+tijana333@users.noreply.github.com>
2026-05-14 18:26:43 +0200

    Flashcard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Flashcard from "./components/Flashcard";
 import cards from "./data/cards";
 import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
+  const [currentCard, setCurrentCard] = useState(0);
+
+  function nextCard() {
+    setCurrentCard(currentCard + 1);
+  }
   return (
-    <div>
-      <Flashcard
-        question="What is React?"
-        answer="A JavaScript library for building UI."
-      />{" "}
+    <div onClick={nextCard}>
+      <h1>{cards[currentCard].question}</h1>
     </div>
   );
 }
